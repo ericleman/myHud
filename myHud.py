481aec7f4aaaa2de5cc095d9efa4799d6269fdd3
ericleman <eric.leman@gmail.com>
2017-11-17 11:52:03 +0100

    Update myHud.py

--- myHud.py
+++ myHud.py
@@ -9,24 +9,25 @@ Dans la table player, on récupère les noms player_name grâce à id_player.
 '''
 import psycopg2
 import psycopg2.extras
+import sys
 from subprocess import getoutput
-from tkinter import ttk
+from  PySide import QtGui, QtCore
 from time import sleep
-import tkinter as tk
 
 DB="dbname='PT4' user='postgres' host='localhost' password='a'"
+SQL_HISTORY = '3 minutes'
 SQL_GET_PLAYER = """SELECT player.player_name,cash_table.table_name,seat,
                       (select seat from cash_table_session_summary C 
-                      where C.id_table=X.id_table and (C.date_end + interval '3 minutes') > (now() at time zone 'UTC')
+                      where C.id_table=X.id_table and (C.date_end + interval '"""+SQL_HISTORY+"""') > (now() at time zone 'UTC')
                       and C.id_player=(select id_player from player where player_name='CookandPoker')) as myseat  
           FROM (SELECT id_table,id_player,seat,row_number() over(partition by seat,id_table order by date_end desc) as rn
-              FROM cash_table_session_summary where (date_end + interval '3 minutes') > (now() at time zone 'UTC')) X,
+              FROM cash_table_session_summary where (date_end + interval '"""+SQL_HISTORY+"""') > (now() at time zone 'UTC')) X,
           cash_table, player
           where cash_table.id_table = X.id_table
           AND player.id_player = X.id_player
           and X.rn = 1
           and (select seat from cash_table_session_summary C 
-                      where C.id_table=X.id_table and (C.date_end + interval '3 minutes') > (now() at time zone 'UTC')
+                      where C.id_table=X.id_table and (C.date_end + interval '"""+SQL_HISTORY+"""') > (now() at time zone 'UTC')
                       and C.id_player=(select id_player from player where player_name='CookandPoker')) is not null"""
 SQL_GET_STATS = """SELECT sum(case when S.flg_vpip then 1 else 0 end) as cnt_vpip,
           sum(case when S.id_hand > 0 then 1 else 0 end) as cnt_hands,
@@ -48,50 +49,8 @@ SQL_GET_STATS = """SELECT sum(case when S.flg_vpip then 1 else 0 end) as cnt_vpi
 conn = psycopg2.connect(DB)
 cur = conn.cursor(cursor_factory = psycopg2.extras.NamedTupleCursor)
 
-class Hud(tk.Toplevel):
-  def __init__(self,parent,player,seat,table):
-    tk.Toplevel.__init__(self,parent)
-    self.player = player
-    self.seat = seat
-    self.table = table
-    self.configure(background='black')
-    self.overrideredirect(True)
-    self.attributes("-topmost", True)
-    self.wait_visibility(self)
-    self.wm_attributes("-alpha", 0.8)
-    self.text = tk.Text(self,height=2, wrap="word",bg="black",fg="white", font=("Roboto", 8))
-    self.text.pack(side="top", fill="x")
-    self.text.tag_configure("yellow", foreground="yellow")
-    self.text.tag_configure("white", foreground="white")
-    self.text.tag_configure("red", foreground="red")
-    self.text.tag_configure("blue", foreground="blue")
-    self.text.tag_configure("green", foreground="green")   
-    #self.label1 = tk.Label(self, bg='black',fg='white', font=("Roboto", 8))
-    #self.label2 = tk.Label(self, bg='black',fg='white', font=("Roboto", 8))
-    #self.label1.grid(row=0,column=0)
-    #self.label2.grid(row=1,column=0)
-    #self.label1.pack(side="right", fill="both", expand=True)
-  def Update(self,stats):
-    X,Y,W,H = get_win_position(self.table)
-    x,y = get_hud_position(self.seat,X,Y,W,H)
-    self.geometry('100x40+'+str(x)+'+'+str(y))
-    self.stats=stats
-    self.text.delete(1.0, tk.END)
-    self.text.insert("end", self.player[:3]+': ' ,"white")
-    self.text.insert("end", '{:2.0f}'.format(stats['vpip']) ,"yellow")
-    self.text.insert("end", ' / ' ,"white")
-    self.text.insert("end", '{:2.0f}'.format(stats['pfr']) ,"yellow")
-    self.text.insert("end", ' / ' ,"white")
-    self.text.insert("end", '{:2.0f}\n'.format(stats['3Bet']) ,"blue")
-    self.text.insert("end", '{:2.0f}'.format(stats['CBet']) ,"green")
-    self.text.insert("end", ' / ' ,"white")
-    self.text.insert("end", '{:2.0f}'.format(stats['CBetF']) ,"green")
-    self.text.insert("end", ' / ' ,"white")
-    self.text.insert("end", '({:2.0f})'.format(stats['hands']) ,"red")
-    #self.label1.configure(text=stats_to_str(self.stats,1))
-    #self.label2.configure(text=stats_to_str(self.stats,2))
-
 def get_win_position(name):
+  X,Y,W,H=0,0,0,0
   id = getoutput("xdotool search --name '"+name+"'")
   lines=getoutput("xwininfo -id "+id).split("\n")
   if lines[0].find("error")>0:
@@ -132,27 +91,19 @@ def divide(a,b):
   else:
     return a/b
 
-def stats_to_str(stats,type):
-  if type==0:
-    return '{:2.0f}'.format(stats['vpip']) + ' / ' + \
-      '{:2.0f}'.format(stats['pfr']) + ' / ' + \
-      '{:2.0f}'.format(stats['3Bet']) + ' / ' + \
-      '{:2.0f}'.format(stats['CBet']) + ' / ' + \
-      '{:2.0f}'.format(stats['CBetF']) + ' / (' + \
-      '{:2.0f}'.format(stats['hands']) + ')'
-  if type==1:
-    return '{:2.0f}'.format(stats['vpip']) + ' / ' + \
-      '{:2.0f}'.format(stats['pfr']) + ' / ' + \
-      '{:2.0f}'.format(stats['3Bet'])
-  if type==2:
-    return '{:2.0f}'.format(stats['CBet']) + ' / ' + \
-      '{:2.0f}'.format(stats['CBetF']) + ' / (' + \
-      '{:2.0f}'.format(stats['hands']) + ')'
+def stats_to_str(stats):
+  return '<center><b><font face="roboto" size="3"><font color="black">'+stats['player'][:3] + ': </font> ' + \
+    '<font color="red">' + '{:1.0f}'.format(stats['vpip']) + '</font><font color="black"> / </font>' + \
+    '<font color="red">' + '{:1.0f}'.format(stats['pfr']) + '</font><font color="black"> / </font>' + \
+    '<font color="blue">' + '{:1.0f}'.format(stats['3Bet']) + '</font><font color="black"><br></font>' + \
+    '<font color="green">' + '{:1.0f}'.format(stats['CBet']) + '</font><font color="black"> / </font>' + \
+    '<font color="green">' + '{:1.0f}'.format(stats['CBetF']) + '</font><font color="black"> / (</font>' + \
+    '<font color="red">' + '{:1.0f}'.format(stats['hands']) + '</font><font color="black">)</font></font></b></center>'
 
 def get_stats(player):
   cur.execute(SQL_GET_STATS.replace('%PLAYER_NAME%',player))
   res = cur.fetchone()
-  stats={'vpip':divide(res.cnt_vpip , (res.cnt_hands - res.cnt_walks)) * 100,
+  stats={'player':player,'vpip':divide(res.cnt_vpip , (res.cnt_hands - res.cnt_walks)) * 100,
     'pfr':divide(res.cnt_pfr , res.cnt_pfr_opp) * 100,
     '3Bet':divide(res.cnt_p_3bet , res.cnt_p_3bet_opp) * 100,
     'CBet':divide(res.cnt_f_cbet , res.cnt_f_cbet_opp) * 100,
@@ -160,37 +111,62 @@ def get_stats(player):
     'hands':res.cnt_hands}  
   return stats
 
-huds=[]
-root = tk.Tk()
-button = tk.Button (root, text = "Good-bye.", command = root.destroy)
-button.pack()
 
-def tick():
-  # get active players from DB (player, seat, table)
-  cur.execute(SQL_GET_PLAYER)
-  players = cur.fetchall()
-  # create a HUD if not exist yet for this player/table
-  for player in players:
-    hasHud = False
-    for hud in huds:
-      if hud.player == player.player_name and hud.table == player.table_name:
-        hasHud = True
-    if not(hasHud):
-      huds.append(Hud(root,player.player_name,(player.seat-player.myseat)%5,player.table_name))
-  # for each HUD, check if it should be removed (because player not active anymore) or moved and update it with the stats
-  for hud in huds:
-    hasPlayer = False
+class Hud(QtGui.QLabel):
+  def __init__(self,player,seat,table):
+    super(Hud,self).__init__()
+    self.player = player
+    self.seat = seat
+    self.table = table
+    self.setWindowFlags(QtCore.Qt.SplashScreen | QtCore.Qt.WindowStaysOnTopHint)
+    self.setWindowOpacity(0.7)
+    #self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+    self.resize(100,40)
+    self.setText('<font face="verdana" color="green">This is some text!</font><br><font face="roboto" color="red">This is some text!</font>')
+    self.show()
+  def Update(self,stats):
+    X,Y,W,H = get_win_position(self.table)
+    x,y = get_hud_position(self.seat,X,Y,W,H)
+    self.move(x,y)
+    self.stats=stats
+    self.setText(stats_to_str(self.stats))
+
+def main():
+  huds=[]
+  def tick():
+    # get active players from DB (player, seat, table)
+    cur.execute(SQL_GET_PLAYER)
+    players = cur.fetchall()
+    # create a HUD if not exist yet for this player/table
     for player in players:
-      if hud.player == player.player_name  and hud.table == player.table_name:
-        hasPlayer = True
-    if not(hasPlayer):
-      hud.destroy()
-      huds.remove(hud)
-    else:
-      # Move it if required and Update Stats
-      stats=get_stats(hud.player)
-      hud.Update(stats)
-  root.after(2000, tick)
+      hasHud = False
+      for hud in huds:
+        if hud.player == player.player_name and hud.table == player.table_name:
+          hasHud = True
+      if not(hasHud):
+        huds.append(Hud(player.player_name,(player.seat-player.myseat)%5,player.table_name))
+    # for each HUD, check if it should be removed (because player not active anymore) or moved and update it with the stats
+    for hud in huds:
+      hasPlayer = False
+      for player in players:
+        if hud.player == player.player_name  and hud.table == player.table_name:
+          hasPlayer = True
+      if not(hasPlayer):
+        huds.remove(hud)
+        hud=None
+      else:
+        # Move it if required and Update Stats
+        stats=get_stats(hud.player)
+        hud.Update(stats)
 
-tick()
-root.mainloop()
+  app=QtGui.QApplication([''])
+  w=QtGui.QWidget()
+  w.resize(200, 5)
+  w.setWindowTitle('HUD')
+  w.show()
+  timer = QtCore.QTimer()
+  timer.timeout.connect(tick)
+  timer.start(1000)
+  sys.exit(app.exec_())
+
+main()
